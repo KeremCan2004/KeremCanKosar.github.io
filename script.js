bf8f135deb382034ffddf6fcd82257eb056700b1
Kerem Can Koşar <keremcankosar@192.168.1.23>
2025-03-28 14:21:52 +0300

    first

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("newData").addEventListener("click", () => {
-    fetch("https://api.chucknorris.io/jokes/random")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("content").innerHTML = `<p>${data.value}</p>`;
-        })
-        .catch(error => console.error("Hata oluştu:", error));
-});
